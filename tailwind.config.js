c86f9ed821a32ea9152e02bb277fb28e73bf870c
Lee Burrell <lee.burrell@ama-assn.org>
2024-07-01 13:15:16 -0400

    incl material and tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,103 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,ts}'],
+	content: ['./src/**/*.html', './src/**/*.ts'],
 	theme: {
-		extend: {}
-	},
-	plugins: []
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: '#000000',
+			white: '#ffffff',
+			'ama-purple': '#46166b', // $purple
+			'ama-purple-85': '#7229d1', // $homepagePurple
+			'ama-purple-60': '#9073a6', //$hoverPurple
+			'ama-purple-20': '#dad0e1', //$bodyTextHoverPurple
+
+			'ama-blue': '#027dbc', // $blue
+			'ama-blue-40': '#0060e6',
+			'ama-blue-80': '#3597c9', // $hoverBlue
+			'ama-blue-10': '#d9f2fc', // $light-blue
+
+			'ama-orange': '#ed8b00', // $orange
+			'ama-orange-80': '#f1a233', // $hoverOrange
+
+			'ama-black': '#000000',
+			'ama-gray-75': '#333333', // ama-gray-75
+			'ama-gray-64': '#5c5c5c', // 'ama-gray-6'4
+			'ama-gray-55': '#717073', // 'ama-gray-6'4
+			'ama-gray-50': '#7f7f7f', // 'ama-gray-5'0
+			'ama-gray-20': '#cbcbcb', //'ama-gray-2'0
+			'ama-gray-15': '#e8e6eb', //'ama-gray-1'5
+			'ama-gray-7': '#ededed',
+			'ama-white': '#ffffff',
+
+			'ama-red': '#e90c26', // $red
+			'ama-red-90': '#d81735', // $hoverRed
+			'ama-green': '#4d8406', // $green
+
+			'freida-black': 'black', //black'
+			'freida-white': 'white', //white
+
+			'freida-gray-95': '#979797',
+			'freida-gray-85': '#545454',
+			'freida-gray-80': '#636370',
+			'freida-gray-60': '#404040',
+			'freida-gray-55': '#4c4b4d', //dark gray
+			'freida-gray-50': '#8b898c', //medium gray
+			'freida-gray-40': '#717073', // light medium gray
+			'freida-gray-25': '#cac8cc', //gray
+			'freida-gray-15': '#e8e6eb', //light gray
+			'freida-gray-13': '#e1e1e6',
+			'freida-gray-10': '#f8f7fa', //off-white'
+			'freida-gray-5': '#dddddd',
+
+			'freida-blue': '#1254b3', //dark blue
+			'freida-blue-90': '#0060e6', //blue
+			'freida-blue-80': '#027dbc', //medium blue
+			'freida-blue-60': '#2356F4',
+			'freida-blue-30': '#d9f2fc', //light ADDS blue
+			'freida-blue-20': '#f2f8ff', //light blue
+			'freida-blue-15': '#d9e9ff', //medium blue / text
+			'freida-blue-10': '#eef7ff', //light blue / background
+			'freida-blue-7': '#dcf2ff',
+			'freida-blue-5': '#DCE9FD', //lighter blue
+			'freida-green': '#00a68a', //dark green
+			'freida-green-70': '#00d9b5', //green
+			'freida-purple': '#451a80', //secondary
+			'freida-purple-90': '#512d6d', //dark primary
+			'freida-purple-80': '#7233cc', //primary
+			'freida-purple-60': '#EFE4FF',
+			'freida-purple-40': '#e1ccff', //medium-purple/text
+			'freida-purple-15': '#896DB1', //light-purple
+			'freida-purple-10': '#E1CCFF', //backdground-purple
+			'freida-purple-5': '#f7f2ff', //light-purple
+			'freida-purple-2': '#ECE0FD', //lighter-purple
+			'freida-purple-1': '#F9F6FE', //lighter-purple
+			'freida-purple-100': '#300964', //dark purple
+			'freida-red': '#ca0828', //red
+			'freida-red-10': '#ad0003',
+			'freida-orange': '#ff4500',
+			'freida-yellow': '#f7b324',
+			'freida-white-opacity-25': '#ffffff25',
+			'freida-survey-background': '#e1ccff'
+		},
+		fontFamily: {
+			sans: ['myriad-pro', 'sans-serif']
+		},
+		fontSize: {
+			xs: '.85rem', //12px
+			sm: '0.875rem', //14px
+			'17px': '1.0625rem', //17px
+			base: '1rem', //20px
+			'med-sm': '1.125rem', //18px
+			med: '1.25rem', //18px
+			'med-lg': '1.333rem', //24px
+			lg: '1.5rem', //26px
+			xl: '1.875rem', //30px
+			'2xl': '2.375rem', //28px
+			'3xl': '2.5rem', //40px
+			'4xl': '3.125rem', //51px
+			'5xl': '3.5rem', //56px
+			'28px': '1.75rem' //28px
+		},
+		important: true
+	}
 };
