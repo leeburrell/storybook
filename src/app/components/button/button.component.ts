fb3702c51feb5d874784ea8a640bd88e5368352f
Lee Burrell <lee.burrell@ama-assn.org>
2024-07-01 12:52:40 -0400

    tailwind storybook scss

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'storybook-button',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule],
+  template: ` <button mat-flat-button class="test">
+    {{ label }}
+  </button>`,
+  styleUrls: ['./button.scss'],
+})
+export class ButtonComponent {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  @Input()
+  primary = false;
+
+  /**
+   * What background color to use
+   */
+  @Input()
+  backgroundColor?: string;
+
+  /**
+   * How large should the button be?
+   */
+  @Input()
+  size: 'small' | 'medium' | 'large' = 'medium';
+
+  /**
+   * Button contents
+   *
+   * @required
+   */
+  @Input()
+  label = 'Button';
+
+  /**
+   * Optional click handler
+   */
+  @Output()
+  onClick = new EventEmitter<Event>();
+
+  public get classes(): string[] {
+    const mode = this.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+    return ['storybook-button', `storybook-button--${this.size}`, mode];
+  }
+}
